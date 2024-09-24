e87bb6f66e43549dcd9c4b5439ec3bb327f32b18
nikolay <nazarov.nikolay15@wb.ru>
2024-09-25 00:58:09 +0300

    main premerge

--- app/src/main/java/com/buytown/ru/data/model/User.kt
+++
@@ -1,4 +0,0 @@
-package com.buytown.ru.data.model
-
-data class User(val username: String, val email: String, val password: String) {
-}
--- app/src/main/java/com/buytown/ru/data/network/LoginResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.buytown.ru.data.network
-
-data class LoginResponse(
-    val access_token: String
-)
